0d62a9c1780c02741dc45d0a6baf716e1e16160f
lobinhodev <contatolobinhodev@gmail.com>
2022-05-22 00:11:53 -0300

    🌈 style:

--- juridico/juridico_front/src/app/components/nav-bar/nav-bar.component.ts
+++ juridico/juridico_front/src/app/components/nav-bar/nav-bar.component.ts
@@ -10,4 +10,12 @@ export class NavBarComponent implements OnInit {
   constructor(router: Router) {}
 
   ngOnInit(): void {}
+
+  title = 'slidebarUi';
+
+  _opened: boolean = false;
+
+  _toggleOpened(): void {
+    this._opened = !this._opened;
+  }
 }
