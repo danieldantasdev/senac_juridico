11adcd3f48629d19d74094ae8f948cca68a88b98
djenole <55561761+djenole@users.noreply.github.com>
2022-06-20 15:45:50 -0300

    primeiro commit

--- juridico_front_angular_material/src/app/historico-soliciatacoes/historico-soliciatacoes.component.ts
+++ juridico_front_angular_material/src/app/historico-soliciatacoes/historico-soliciatacoes.component.ts
@@ -24,7 +24,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   {position: 3, name: 'ProcessoGGI', responsavel: 'Q W S', setor: 'R.H.', dataProcesso:'04/01/2022', status: 'Em processo'},
   {position: 4, name: 'ProcessoQWS', responsavel: 'F E D', setor: 'R.H.', dataProcesso:'12/07/2022', status: 'Em processo'},
   {position: 5, name: 'ProcessoHGG', responsavel: 'V D C', setor: 'jurídico', dataProcesso:'03/05/2021', status: 'Em processo'},
-  {position: 6, name: 'ProcessoERE', responsavel: 'G F', setor: 'jurídico', dataProcesso:'15/01/2022', status: 'Em processo'},
+  {position: 6, name: 'ProcessoERE', responsavel: 'G F A', setor: 'jurídico', dataProcesso:'15/01/2022', status: 'Em processo'},
   {position: 7, name: 'ProcessoTEW', responsavel: 'S S C', setor: 'Administrativo', dataProcesso:'12/12/2022', status: 'Em processo'},
   {position: 8, name: 'ProcessoYTU', responsavel: 'Q Z S', setor: 'Administrativo', dataProcesso:'06/07/2021', status: 'Em processo'},
   {position: 9, name: 'ProcessoMNV', responsavel: 'V C S', setor: 'R.H.', dataProcesso:'05/09/2022', status: 'Em processo'},
